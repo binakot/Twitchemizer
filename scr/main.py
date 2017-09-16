54ed525c4d5380194b3664009e463e571b2124a5
Ivan Muratov <binakot@gmail.com>
2017-09-16 22:57:04 +0300

    Added single watcher on twtich channel via livestreamer. Also added livestreamer library complete example.

--- scr/main.py
+++ scr/main.py
@@ -1,1 +1,20 @@
-print("Hello, World!")
+import sys
+from livestreamer import Livestreamer
+
+session = Livestreamer()
+session.set_loglevel("info")
+session.set_logoutput(sys.stdout)
+session.set_option("http-headers", "Client-ID=jzkbprff40iqj646a697cyrvl0zt2m6")
+
+streams = session.streams('http://twitch.tv/blizzakot')
+stream = streams['audio_only']
+
+# fd = stream.open()  # read to file
+# with open("../tmp/stream.dat", 'wb') as f:
+#     while True:
+#         data = fd.read(1024)
+#         f.write(data)
+
+fd = stream.open()  # read to nowhere
+while True:
+    fd.read(1024)
