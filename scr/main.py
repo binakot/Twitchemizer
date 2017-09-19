d1c4a780e44bc4fa9cc358b9cb0b4451747d9de2
Ivan Muratov <binakot@gmail.com>
2017-09-19 19:39:20 +0300

    Added running parallel watchers in separated threads.

--- scr/main.py
+++ scr/main.py
@@ -1,5 +1,9 @@
-import LiveStreamerWatcher
+import thread
 import StreamLinkWatcher
 
-# LiveStreamerWatcher.watch('http://twitch.tv/blizzakot')  # TODO async
-StreamLinkWatcher.watch('http://twitch.tv/blizzakot')
+WATCHER_COUNT = 10
+
+for x in range(0, WATCHER_COUNT):
+    thread.start_new_thread(StreamLinkWatcher.watch, ('https://www.twitch.tv/blizzakot',))
+
+raw_input("Press Enter to quit...")
